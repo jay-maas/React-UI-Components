4c2aa1bb6a88d30611a042b900356394619b0826
Jay Maas <jaymaas.dev@gmail.com>
2019-04-22 17:33:25 -0600

    Footer Partial Complete
    Images and stylings in place. Need to update with counters

--- Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import './Footer.css';
 
+const clickButton = function(e) {
+  e.preventDefault();
+  alert("This Function is not fully supported...Yet")
+}
+
 const Footer = () => {
     return (
       <div className="footer">
-        <img src="" alt="message"/>
-        <img src="" alt="refresh"/>
-        <img src="" alt="love"/>
-        <img src="" alt="mail"/>
+        <img onClick={clickButton} className="click" src="assets/message.png" alt="footer" />
+        <img onClick={clickButton} className="click" src="assets/refresh.png" alt="footer" />
+        <img onClick={clickButton} className="click" src="assets/heart.png" alt="footer" />
+        <img onClick={clickButton} className="click" src="assets/mail.png" alt="footer" />
       </div>
     );
   };
   
   export default Footer;
-  
