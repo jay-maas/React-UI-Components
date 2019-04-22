3cf5dede3242dfb9c32a68ce54de9b499c3b996b
Jay Maas <jaymaas.dev@gmail.com>
2019-04-22 15:59:34 -0600

    Card Content MVP complete
    Per the design file, the card content MVP has been achieved. Working towards stretch goals.

--- Project-I-Social-Card/social-card/src/App.js
+++ Project-I-Social-Card/social-card/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import HeaderContainer from './components/HeaderComponents/HeaderContainer';
+import CardContainer from './components/CardComponents/CardContainer';
 import './App.css';
 
 
@@ -7,6 +8,7 @@ const App = () => {
   return (
     <div className="app">
       <HeaderContainer />
+      <CardContainer />
     </div>
   );
 };
--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,26 @@
 import React from 'react';
+import CardBanner from './CardBanner';
 import './Card.css';
+
+const CardContent = () => {
+    return (
+      <a href="https://www.reactjs.org" alt="link whole card to website" className="cardContent">
+          <p>Let's learn React by building simpole interfaces with components. Don't try to 
+            overthink it, just keep it simple and have fun. Once you feel comfortable using
+            components you are well on your way to mastering React!
+          </p>
+
+        <CardBanner />
+
+        <div className="belowBanner">
+          <h3>Get started with React</h3>
+          <p>React makes it painless to create interactive UIs. Design simple views for
+            each state in our application.
+          </p>
+          <a href="reactjs.org" alt="link to react website">reactjs.org</a>
+        </div>
+      </a>
+    );
+  };
+  
+  export default CardContent;
