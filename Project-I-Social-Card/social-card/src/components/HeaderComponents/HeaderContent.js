ce91a1af6aabf3dd27307d2978019f76b74b50ea
Jay Maas <jaymaas.dev@gmail.com>
2019-04-22 14:54:27 -0600

    Header MVP complete
    Using the components constructed the header as desired.

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
@@ -1,2 +1,15 @@
 import React from 'react';
+import HeaderTitle from './HeaderTitle';
+import ImageThumbnail from './ImageThumbnail';
 import './Header.css';
+
+const HeaderContent = () => {
+    return (
+      <div className="headerContent">
+          <ImageThumbnail />
+          <HeaderTitle />
+      </div>
+    );
+  };
+  
+  export default ImageThumbnail;
